e3d5fe492c600d624f2a605c92a5c7a902095dcc
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2020-11-29 18:42:35 +0200

    add new tests

--- src/test/java/bgu/spl/mics/MessageBusTest.java
+++ src/test/java/bgu/spl/mics/MessageBusTest.java
@@ -2,6 +2,7 @@ package bgu.spl.mics;
 
 import bgu.spl.mics.application.messages.AttackEvent;
 import bgu.spl.mics.application.passiveObjects.Ewok;
+import bgu.spl.mics.application.services.C3POMicroservice;
 import bgu.spl.mics.application.services.HanSoloMicroservice;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
@@ -33,7 +34,7 @@ public class MessageBusTest {
         Future<Boolean> future1 = messageBus.sendEvent(event);
         Future<Boolean> future2 = messageBus.sendEvent(event);
         future1.resolve(true);
-        future2.resolve(true);
+        future2.resolve(false);
         assertTrue(future1.get() && !future2.get());
     }
 
@@ -45,6 +46,35 @@ public class MessageBusTest {
         Future<Boolean> future = messageBus.sendEvent(event);
         Message message = messageBus.awaitMessage(hanSolo);
         assertEquals(event , message);
+    }
+
+    @Test
+    public void testComplete(){
+        HanSoloMicroservice hanSolo = new HanSoloMicroservice();
+        messageBus.register(hanSolo);
+        Event<Boolean> event = new AttackEvent();
+        Future<Boolean> future = messageBus.sendEvent(event);
+        future.resolve(true);
+        messageBus.complete(event , true);
+        assertTrue(future.isDone());
+    }
+
+    @Test
+    public void multiMicroService_MultiEvent(){
+        HanSoloMicroservice hanSolo = new HanSoloMicroservice();
+        C3POMicroservice C3PO = new C3POMicroservice();
+        messageBus.register(hanSolo);
+        messageBus.register(C3PO);
+        Event<Boolean> event1 = new AttackEvent();
+        Event<Boolean> event2 = new AttackEvent();
+        Future<Boolean> future1 = messageBus.sendEvent(event1);
+        Future<Boolean> future2 = messageBus.sendEvent(event2);
+        future1.resolve(true);
+        messageBus.complete(event1 , true);
+        future2.resolve(true);
+        messageBus.complete(event2 , true);
+        assertTrue(future1.isDone() && future2.isDone());
+        assertNotEquals(future1.get(),future2.get());
     }
 
 }
