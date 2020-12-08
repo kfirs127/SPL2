412e5a836567992a696369d450723ac25a2af87e
BARDAMRI <batdam@post.bgu.ac.il>
2020-12-08 22:21:16 +0200

    read from json

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,4 +1,6 @@
 package bgu.spl.mics.application;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,11 +9,15 @@ import java.util.LinkedList;
 import bgu.spl.mics.application.passiveObjects.Attack;
 import bgu.spl.mics.application.passiveObjects.Diary;
 import bgu.spl.mics.application.passiveObjects.Ewoks;
+import bgu.spl.mics.application.passiveObjects.Input;
 import bgu.spl.mics.application.services.*;
+import com.google.gson.Gson;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import javax.xml.transform.Result;
 
 /** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
@@ -19,41 +25,22 @@ import org.json.simple.parser.ParseException;
  */
 public class Main {
 	public static void main(String[] args) throws IOException {
-		//Creating a JSONParser object
-		JSONParser jsonParser = new JSONParser();
-		String inputDirection = args[0];
-		String contents = new String((Files.readAllBytes(Paths.get(inputDirection))));
-		Attack[] attacks;
-		Diary diary=Diary.getInstance();
-		int place=0;
-		JSONObject jsonObject=null;
-		try {
-			 jsonObject = (JSONObject) jsonParser.parse(contents);
+		Gson gson=new Gson();
+		BufferedReader reader=new BufferedReader(new FileReader(args[0]));
+		Input attackobj= gson.fromJson(reader, Input.class);
+		long ewoks=attackobj.getEwoks();
+		long landoD=attackobj.getLando();
+		long r2d2D=attackobj.getR2D2();
+		Attack [] attacks=new Attack[attackobj.getAttacks().length];
+		int i=0;
+		for(Attack attack : attackobj.getAttacks()) {
+			attacks[i]=new Attack(attack.getSerials(),attack.getDuration());
+			i++;
 		}
-		catch (ParseException p){}
-		if(jsonObject==null)
-			throw new NullPointerException("couldn read json input");
-			Long r2d2D=(Long) jsonObject.get("R2D2");
-			Long landoD=(Long) jsonObject.get("Lando");
-			Long ewoksN=(Long) jsonObject.get("Ewoks");
-			JSONArray attacksArray = (JSONArray) jsonObject.get("attacks");
-			attacks = new Attack[attacksArray.size()];
-			Iterator<JSONObject> iterator = attacksArray.iterator(); //Iterating the contents of the array
-			while (iterator.hasNext()) {
-				JSONObject objec = (JSONObject) iterator.next();
-				JSONArray ewoks = (JSONArray) objec.get("serials");
-				LinkedList<Integer> serial = new LinkedList<>();
-				for (int i = 0; i < ewoks.size(); i++) {
-					Long temp = (Long) ewoks.get(i);
-					serial.add(temp.intValue());
-				}
-				attacks[place] = new Attack(serial, ((Long) objec.get("duration")).intValue());
-				place++;
-			}
-			Ewoks supply=Ewoks.getInstance();
-			for(int i=0;i<ewoksN;i++){
-				supply.addEwok();
-			}
+		Ewoks supply=Ewoks.getInstance();
+		for(i=0;i<ewoks;i++){
+			supply.addEwok();
+		}
 			C3POMicroservice c3po=new C3POMicroservice();
 			HanSoloMicroservice hanSolo=new HanSoloMicroservice();
 			LeiaMicroservice leia = new LeiaMicroservice(attacks);
